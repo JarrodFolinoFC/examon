abe55f4eaa74c5675d3c7d431066952d228f7944
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-08-28 21:11:18 +0200

    refactor

--- examon/lib/pip_installer.py
+++ examon/lib/pip_installer.py
@@ -3,16 +3,23 @@ import sys
 import importlib.util
 import logging
 
+from examon.lib.config.settings_manager_factory import SettingsManagerFactory
+
 
 class PipInstaller:
+
     @staticmethod
-    def install(packages):
-        logging.info(f"installing {len(packages)} repos")
-        for package in packages:
+    def install(examon_config):
+        path = examon_config.config_full_file_path()
+        manager = SettingsManagerFactory.build(path)
+        logging.info(f"installing {len(manager.packages)} repos")
+        for package in manager.packages:
             with open('pip_install.log', "w") as outfile:
                 cmd = [sys.executable, "-m", "pip", "install", package['name'], '--upgrade']
                 logging.debug(cmd)
                 subprocess.run(cmd, stdout=outfile)
+        PipInstaller.import_packages(manager.active_packages)
+        return manager
 
     @staticmethod
     def is_package_installed(package):
--- examon/view/cli/overview.py
+++ examon/view/cli/overview.py
@@ -1,6 +1,6 @@
 from examon.lib.reporting.stats import Stats
 from examon.lib.config.settings_manager_factory import SettingsManagerFactory
-from examon.lib.config.json_config_factory import JsonConfigFactory
+from examon.lib.pip_installer import PipInstaller
 
 from examon.lib.storage.question_factory import QuestionFactory
 
@@ -14,7 +14,7 @@ class OverviewCli:
         config = ConfigStructureFactory.default_config()
         ValidateConfig.config_dir_exists(config)
 
-        JsonConfigFactory.install_packages(config)
+        PipInstaller.install(config)
         SettingsManagerFactory.build(config.config_full_file_path())
         print('overview')
         manager = SettingsManagerFactory.build(config.config_full_file_path())
