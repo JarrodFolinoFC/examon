d8d72a00320480bd66815295af98a30f57c5a05b
Jarrod Folino <jarrod.folino@fundingcircle.com>
2023-08-29 17:21:42 +0200

    Remove code not needed

--- examon/lib/storage/examon_writer_factory.py
+++ examon/lib/storage/examon_writer_factory.py
@@ -13,6 +13,13 @@ from .drivers.content.sql_db import QuestionQuery
 class ExamonWriterFactory:
     @staticmethod
     def build(base_dir, db_name, models):
+        if not os.path.isfile(base_dir):
+            Path(base_dir).mkdir(parents=True, exist_ok=True)
+
+        if not os.path.isfile(db_name):
+            root_dir = os.path.abspath(os.curdir)
+            shutil.copyfile(f'{root_dir}/resources/examon.db', db_name)
+
         engine = create_engine(f"sqlite+pysqlite:///{db_name}", echo=True)
 
         ids = QuestionQuery(engine).question_unique_ids()
